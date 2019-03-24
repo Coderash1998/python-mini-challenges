081d943baae6ea2738531c5fd3455485f56487c3
Vallabh Ashish Kedarisetti <ashish.vallabh16@siesgst.ac.in>
2019-03-24 12:12:58 +0530

    Code file created.

---
+++ code.py
@@ -0,0 +1,82 @@
+# --------------
+#Code starts here
+import sys
+def palindrome(num):
+    numstr = str(num)
+    for i in range(num+1,sys.maxsize):
+        if str(i) == str(i)[::-1]:
+            return i
+#print(palindrome(13456))
+
+
+# --------------
+#Code starts here
+#Code starts here
+
+#Function to find anagram of one word in another
+
+def a_scramble(str_1,str_2):
+    result=True
+    for i in (str_2.lower()):
+        if i not in (str_1.lower()):
+            result=False
+            break
+        str_1=str_1.replace(i,'',1) #Removing the letters from str_1 that are already checked
+    
+    return (result)
+
+#Code ends here
+
+
+# --------------
+#Code starts here
+import math
+def CheckPerfect(x):
+  i = int(math.sqrt(x))
+  return (x == i*i)
+
+def check_fib(num):
+  if(CheckPerfect(5*num*num + 4) or CheckPerfect(5*num*num - 4)):
+    return True
+  else:
+    return False
+
+
+
+
+
+# --------------
+#Code starts here
+
+#Function to compress string
+def compress(word):
+    word=word.lower()
+    mist=[]
+    l=0
+    while(l<len(word)):
+        m=word[l]
+        j=0
+        while(l<len(word) and word[l]==m):
+                 j=j+1
+                 l=l+1    
+
+        mist.append(m)
+        mist.append(str(j))
+    
+    return ''.join(mist)
+
+#Code ends here
+
+
+# --------------
+#Code starts here
+#Code starts here
+
+#Function to check existence of k distinct characters in string
+def k_distinct(string,k):
+    s_list=(set(string.lower()))
+    return len(s_list)>=k
+
+#Code ends here
+
+
